d74c83e9c2155a488269e230a516f41ab7345d35
mamort <matsmortensen@gmail.com>
2015-12-21 15:23:21 +0100

    Initial

---
+++ Gjesteparkering/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace Gjesteparkering
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Content/Components/jquery/dist/jquery.js",
+                        "~/Content/Components/react/react.js",
+                        "~/Content/Components/react/react-dom.js",
+                        "~/Content/Components/bootstrap/dist/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Content/Components/bootstrap/dist/css/bootstrap.css",
+                      "~/Content/css/site.css"));
+        }
+    }
+}
