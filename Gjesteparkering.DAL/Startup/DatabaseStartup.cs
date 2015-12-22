d2f8bf740befcc47545ca665af44f9ed95c216d1
mamort <matsmortensen@gmail.com>
2015-12-22 13:48:25 +0100

    Prøver pånytt å fikse problemet med manglende Entity Framework DLL

--- Gjesteparkering.DAL/Startup/DatabaseStartup.cs
+++ Gjesteparkering.DAL/Startup/DatabaseStartup.cs
@@ -9,11 +9,16 @@ namespace Gjesteparkering.DAL
     {
         private static readonly ILog _log = LogManager.GetLogger(typeof(DatabaseStartup));
 
+
         public void Start()
         {
             _log.Info("Database startup");
-            // Reference dlls that are indirectly referenced and not always included in build
-            var x = typeof(System.Data.Entity.SqlServer.SqlProviderServices);
+
+            //The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+            //for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
+            //Make sure the provider assembly is available to the running application. 
+            //See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
 
             var context = new GjesteparkeringContext();
             var initializeDomain = new CreateDatabaseIfNotExists<GjesteparkeringContext>();
